3cf379f607825f5049f287c308bb5890162fbb9b
Lucas Ha <tuancntt971@gmail.com>
2024-06-02 16:35:24 +0700

    fix build

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/**
+ * Configuration for type-safe environment variables.
+ * Imported through src/app/page.tsx
+ * @see https://x.com/mattpocockuk/status/1760991147793449396
+ */
 import { z } from 'zod';
 
 const envVariables = z.object({
@@ -8,6 +14,7 @@ envVariables.parse(process.env);
 
 declare global {
   namespace NodeJS {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface ProcessEnv extends z.infer<typeof envVariables> {}
   }
 }
